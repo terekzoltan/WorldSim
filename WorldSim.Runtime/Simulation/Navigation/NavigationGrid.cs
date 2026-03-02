f304c555146bde140ded9ced77cc8d43abc2f222
Terék Zoltán Tamás <zoltan.t.terek@gmail.com>
2026-03-02 17:33:41 +0100

    Track navigation topology changes

--- WorldSim.Runtime/Simulation/Navigation/NavigationGrid.cs
+++ WorldSim.Runtime/Simulation/Navigation/NavigationGrid.cs
@@ -3,6 +3,8 @@ namespace WorldSim.Simulation.Navigation;
 public sealed class NavigationGrid
 {
     private readonly World _world;
+    private int _topologyVersion;
+    private int _lastTopologySignature = int.MinValue;
 
     public NavigationGrid(World world)
     {
@@ -11,7 +13,20 @@ public sealed class NavigationGrid
 
     public int Width => _world.Width;
     public int Height => _world.Height;
-    public int TopologyVersion => _world.NavigationTopologyVersion;
+    public int TopologyVersion
+    {
+        get
+        {
+            int signature = ComputeTopologySignature();
+            if (signature != _lastTopologySignature)
+            {
+                _lastTopologySignature = signature;
+                _topologyVersion++;
+            }
+
+            return _topologyVersion;
+        }
+    }
 
     public bool InBounds(int x, int y)
         => x >= 0 && y >= 0 && x < _world.Width && y < _world.Height;
@@ -31,5 +46,32 @@ public sealed class NavigationGrid
             return true;
 
         return false;
+    }
+
+    private int ComputeTopologySignature()
+    {
+        unchecked
+        {
+            int hash = 17;
+            hash = (hash * 31) + _world.Width;
+            hash = (hash * 31) + _world.Height;
+
+            foreach (var house in _world.Houses)
+            {
+                hash = (hash * 31) + house.Pos.x;
+                hash = (hash * 31) + house.Pos.y;
+                hash = (hash * 31) + house.Owner.Id;
+            }
+
+            foreach (var building in _world.SpecializedBuildings)
+            {
+                hash = (hash * 31) + building.Pos.x;
+                hash = (hash * 31) + building.Pos.y;
+                hash = (hash * 31) + building.Owner.Id;
+                hash = (hash * 31) + (int)building.Kind;
+            }
+
+            return hash;
+        }
     }
 }
